0364769e2c5ebf46fa8f84bb7f975c856e5717fd
pjc <desk@pjc.is>
2020-11-06 10:04:46 -0500

    Prepare for GitHub upload

--- filter.php
+++ filter.php
@@ -11,6 +11,7 @@ Feed::$cacheExpire = '+1 hour';
 
 $url = 'https://bpl.bibliocommons.com/events/rss/all';
 $rss = Feed::loadRss($url);
+#$rss->registerXPathNamespace('bc', 'http://bibliocommons.com/rss/1.0/modules/event/');
 
 # https://github.com/zcontent/icalendar
 require_once 'lib/zapcallib.php';
@@ -43,7 +44,11 @@ function set_date($req)
 
     # Radio button options
     # Only without manual date entry
-    if (!$req->start_date || !$req->end_date) {
+    if ($req->start_date || $req->end_date) {
+        # Partial manual date fallback
+        $req->start_date = ($req->start_date) ?: $today;
+        $req->end_date = ($req->end_date) ?: $next_year;
+    } else {
         switch ($req->date_radio) {
           case 'today':
             $req->start_date = $today;
@@ -70,10 +75,6 @@ function set_date($req)
             $req->end_date = $next_year;
             break;
         }
-    } else {
-        # Partial manual date fallback
-        $req->start_date = ($req->start_date) ?: $today;
-        $req->end_date = ($req->end_date) ?: $next_year;
     }
 
     unset($req->date_radio);
@@ -93,17 +94,22 @@ function set_categories($req)
         }
     }
 
+    #var_dump($req);
     return $req;
 }
 
+
+/**
+ * Find matches
+ * todo: Split into 3 clear mini-functions
+ */
+
 # Compare $req and $rss
 # e.g., foreach $match: $ics->export();
-function filter_feed($req, $rss)
+function filter_categories($req, $rss)
 {
-    # Populate $req->matches with SimpleXMLElements
     $req->matches = [];
 
-    # Categories
     # Add all possible matches
     if (empty($req->category)) {
         $req->matches = $rss;
@@ -117,43 +123,52 @@ function filter_feed($req, $rss)
         }
     }
 
-    # Date
-    # Subtract invalid matches
-    # todo
+    #var_dump($req->matches);
+    return $req->matches;
+}
 
+# Filter by radio buttons
+# todo: Make it independent of filter_categories() output
+function filter_options($req, $rss)
+{
+    # Prevent warnings
+    $req->is_virtual = (isset($req->is_virtual)) ?: false;
+    $req->is_featured = (isset($req->is_featured)) ?: false;
+    $req->is_cancelled = (isset($req->is_cancelled)) ?: false;
 
-    # Filter By
     # Subtract invalid matches
     # https://stackoverflow.com/a/622363
-    foreach ($req->matches as $match) {
-        # Prevent warnings
-        $req->is_virtual = (isset($req->is_virtual)) ?: false;
-        $req->is_featured = (isset($req->is_featured)) ?: false;
-        $req->is_cancelled = (isset($req->is_cancelled)) ?: false;
+    if (!empty($req->matches)) {
+        foreach ($req->matches->item as $match) {
+            $ns = $match->children('bc', true);
+            #var_dump($ns);
 
-        # is_virtual mismatch
-        if ($req->is_virtual
-        && !$match->xpath('//bc:is_virtual')) {
-            unset($req->match);
-        }
+            # is_virtual mismatch
+            if ($req->is_virtual && !$ns->is_virtual) {
+                unset($req->$match);
+                var_dump($match);
+            }
 
-        # is_featured mismatch
-        if ($req->is_featured
-        && !$match->xpath('//bc:is_featured')
-        || !$match->xpath('//bc:is_featured_at_location')) {
-            unset($req->match);
-        }
+            # is_featured mismatch
+            if ($req->is_featured && !$ns->is_featured || !$ns->is_featured_at_location) {
+                unset($req->$match);
+            }
 
-        # is_cancelled mismatch
-        # Note default logic: hide cancelled
-        if (!$req->is_cancelled
-                && $match->xpath('//bc:is_cancelled')) {
-            unset($req->match);
+            # is_cancelled mismatch
+            # Note default logic: hide cancelled
+            if (!$req->is_cancelled && $ns->is_cancelled) {
+                unset($req->$match);
+            }
         }
     }
 
     var_dump($req->matches);
-    return $req;
+    return $req->matches;
+}
+
+function filter_date($req, $rss)
+{
+    return false;
 }
 
 
@@ -169,7 +184,8 @@ function filter_feed($req, $rss)
  */
 
 echo '<pre>';
-#var_dump($_POST);
 #var_dump($req);
-var_dump(filter_feed($req, $rss));
+#var_dump($rss);
+filter_categories($req, $rss);
+filter_options($req, $rss);
 echo '</pre>';
--- index.php
+++ index.php
@@ -11,6 +11,12 @@ $meta = new Metadata();
 <head>
   <title>Export Events | Boston Public Library</title>
   <meta charset='utf-8'>
+
+  <style>
+    fieldset {
+      margin-bottom: 1em;
+    }
+  </style>
 </head>
 
 <body>
@@ -19,63 +25,75 @@ $meta = new Metadata();
   <form action='filter.php' method='get'>
 
     <!-- Date -->
-    <h2>Date</h2>
+    <fieldset>
+      <legend>Date</legend>
 
-    <input type='radio' name='date_radio' id='date_radio' value='today' />
-    <label for='today'>Today</label><br />
+      <input type='radio' name='date_radio' id='date_radio' value='today' />
+      <label for='today'>Today</label><br />
 
-    <input type='radio' name='date_radio' id='date_radio' value='tomorrow' />
-    <label for='tomorrow'>Tomorrow</label><br />
+      <input type='radio' name='date_radio' id='date_radio' value='tomorrow' />
+      <label for='tomorrow'>Tomorrow</label><br />
 
-    <input type='radio' name='date_radio' id='date_radio' value='this_weekend' />
-    <label for='this_weekend'>This Weekend</label><br />
+      <input type='radio' name='date_radio' id='date_radio' value='this_weekend' />
+      <label for='this_weekend'>This Weekend</label><br />
 
-    <input type='radio' name='date_radio' id='date_radio' value='next_week' />
-    <label for='next_week'>Next 7 Days</label><br />
+      <input type='radio' name='date_radio' id='date_radio' value='next_week' />
+      <label for='next_week'>Next 7 Days</label><br />
 
-    From
-    <input type='date' name='start_date' id='start_date'
-      min='<?= $today ?>' value='' /><br />
+      From
+      <input type='date' name='start_date' id='start_date'
+        min='<?= $today ?>' value='' /><br />
 
-    To
-    <input type='date' name='end_date' id='end_date'
-      max='<?= strftime('%Y-%m-%d', strtotime('+1 year')) ?>'
-      value='' /><br />
-
-    <!-- Options -->
-    <h2>Filter By</h2>
-
-    <input type="checkbox" id="is_virtual" name="is_virtual" value="true" />
-    <label for="is_virtual">Online Events</label><br />
-
-    <input type="checkbox" id="is_featured" name="is_featured" value="true" />
-    <label for="is_featured">Featured Events</label><br />
-
-    <input type="checkbox" id="is_cancelled" name="is_cancelled" value="true" />
-    <label for="is_cancelled">Cancelled Events</label><br />
+      To
+      <input type='date' name='end_date' id='end_date'
+        max='<?= strftime('%Y-%m-%d', strtotime('+1 year')) ?>'
+        value='' /><br />
+    </fieldset>
 
     <!-- Locations -->
-    <h2>Locations</h2>
-    <?= $meta->make_checkboxes('Locations', $meta->locations) ?>
+    <fieldset>
+      <legend>Locations</legend>
+      <?= $meta->make_checkboxes('Locations', $meta->locations) ?>
+    </fieldset>
 
     <!-- Event Types -->
-    <h2>Event Types</h2>
-    <?= $meta->make_checkboxes('Event Types', $meta->event_types) ?>
+    <fieldset>
+      <legend>Event Types</legend>
+      <?= $meta->make_checkboxes('Event Types', $meta->event_types) ?>
+    </fieldset>
 
     <!-- Programs -->
-    <h2>Programs</h2>
-    <?= $meta->make_checkboxes('Programs', $meta->programs) ?>
+    <fieldset>
+      <legend>Programs</legend>
+      <?= $meta->make_checkboxes('Programs', $meta->programs) ?>
+    </fieldset>
 
     <!-- Audiences -->
-    <h2>Audiences</h2>
-    <?= $meta->make_checkboxes('Audiences', $meta->audiences) ?>
+    <fieldset>
+      <legend>Audiences</legend>
+      <?= $meta->make_checkboxes('Audiences', $meta->audiences) ?>
+    </fieldset>
 
     <!-- Languages -->
-    <h2>Languages</h2>
-    <?= $meta->make_checkboxes('Languages', $meta->languages) ?>
+    <fieldset>
+      <legend>Languages</legend>
+      <?= $meta->make_checkboxes('Languages', $meta->languages) ?>
+    </fieldset>
 
+    <!-- Options -->
+    <fieldset>
+      <legend>Filter By</legend>
 
-    <br /><br />
+      <input type="checkbox" id="is_virtual" name="is_virtual" value="true" />
+      <label for="is_virtual">Online Events</label><br />
+
+      <input type="checkbox" id="is_featured" name="is_featured" value="true" />
+      <label for="is_featured">Featured Events</label><br />
+
+      <input type="checkbox" id="is_cancelled" name="is_cancelled" value="true" />
+      <label for="is_cancelled">Cancelled Events</label><br />
+    </fieldset>
+
     <input type='submit' value='Submit'>
   </form>
 
